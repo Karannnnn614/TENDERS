2d48b1448c805f3d9d6508bbf3b7b30d6e3ca53c
Karan Mundre <karanmundre73@gmail.com>
2025-01-18 01:09:55 +0530

    changes in API Url

--- UI/src/APIUrl.js
+++ UI/src/APIUrl.js
@@ -1,7 +1,7 @@
-export const _userapiurl = `${process.env.TENDERS_SERVER_URI}user/`;
+export const _userapiurl = `${process.env.TENDERS_SERVER_URI}/user/`;
 
-export const _categoryapiurl = `${process.env.TENDERS_SERVER_URI}category/`;
+export const _categoryapiurl = `${process.env.TENDERS_SERVER_URI}/category/`;
 
-export const _subcategoryapiurl = `${process.env.TENDERS_SERVER_URI}subcategory/`;
+export const _subcategoryapiurl = `${process.env.TENDERS_SERVER_URI}/subcategory/`;
 
-export const _paymentapiurl=`${process.env.TENDERS_SERVER_URI}payment`;
+export const _paymentapiurl=`${process.env.TENDERS_SERVER_URI}/payment`;
